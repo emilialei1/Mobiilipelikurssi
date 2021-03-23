313e37ab2e93a8ea22f6e3831b541318f0eb725f
emilia <60214848+emilialei1@users.noreply.github.com>
2021-03-23 09:03:54 +0200

    Added MainMenu

---
+++ Match-3/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
